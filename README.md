45033ba02148acd64ed53a7fdcbe2a73d02f8f19
Stephen R. van den Berg <srb@cuci.nl>
2019-04-08 16:31:44 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,27 @@
+<h1>wspromise</h1>
+
+wspromise is a low-fat-promisified ws-websocket library.
+
+## Requirements
+
+It runs inside any webbrowser or node environment that supports promises.
+
+It has a single dependency: ws.
+
+## Basic usage
+
+The following sample code will illustrate the point:
+
+```js
+```
+
+## Reference documentation
+
+
+### API
+
+Exposed API-list:
+- `ws.open()`<br />
+  ...
+
+## References
---
+++ index.js
@@ -0,0 +1,79 @@
+!function(W)
+{ "use strict";
+
+  function g(ws) {
+    this.ws = ws;
+    this.openprom = new Promise((res, rej) => {
+      this.reject = rej;
+      ws.on("open", () => res());
+    });
+    this.qin = [];
+    ws.on("message", (data) => {
+      var res;
+      if (res = this.recvres)
+        this.recvres = 0, res(data);
+      else
+        this.qin.push(data);
+    });
+    ws.on("error", this.error);
+    ws.on("ping", (data) => {
+      try { ws.pong(data); } catch (e) { this.error(e); }
+    });
+    ws.on("pong", (data) => this.pingres(data));
+  }
+
+  g.prototype.error = function(Error) {
+    try { this.reject(Error); } catch (e) { }
+  };
+
+  g.prototype.open = function() {
+    var prom = this.openprom;
+    this.openprom = 0;
+    return prom;
+  };
+
+  g.prototype.send = function(data, options) {
+    return new Promise((res, rej) => {
+      this.reject = rej;
+      try {
+        this.ws.send(data, options, () => res());
+      } catch (e) { rej(e); }
+    });
+  };
+
+  g.prototype.recv = function() {
+    if (this.qin.length)
+      return this.qin.shift();
+    return new Promise((res, rej) => {
+      this.reject = rej;
+      return this.recvres = res;
+    });
+  };
+
+  g.prototype.ping = function(data) {
+    return new Promise((res, rej) => {
+      this.reject = rej;
+      this.pingres = res;
+      try {
+        this.ws.ping(data);
+      } catch (e) { rej(e); }
+    });
+  };
+
+  g.prototype.close = function(code, reason) {
+    return new Promise((res, rej) => {
+      this.reject = rej;
+      try {
+        this.ws.close(code, reason);
+      } catch (e) { rej(e); }
+      this.ws.on("close", () => res());
+    });
+  };
+
+  if (typeof define == "function" && define.amd)
+    define("wspromise", [], g);
+  else if (W.exports)
+    W.exports.wspromise = g;
+  else
+    W.wspromise = g;
+}(typeof window == "object" ? window : global);
